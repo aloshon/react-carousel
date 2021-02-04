f7fa7d2519e6e62b89431810e309809707ee5dad
Allen Loshon <allenlosho913@gmail.com>
2021-02-03 18:37:57 -0800

    removed extra files and comments

--- src/Card.test.js
+++ src/Card.test.js
@@ -17,9 +17,3 @@ test('playing with queries', () => {
     console.log(getByText('Image', {exact: false}));
     console.log(queryByText('Image', {exact: false}));
 });
-
-// it('should show h4 on load', () => {
-//     const {getByClassName} = render(<Card/>);
-//     const h4 = getByClassName("Card-title");
-//     expect(h4).toBeInTheDocument();
-// })
